9a2a60097b613fa067ea2fe245b0a87b9157383e
xyoraw <2064307115@qq.com>
2021-04-17 17:25:26 +0800

    Add project files.

---
+++ Test_System01/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Test_System01
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyFrame.Navigate(typeof(onePage));
+            chooseRadioButton.IsChecked = true;
+
+        }
+
+        private void chooseRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MyFrame.Navigate(typeof(onePage));
+            checkButton.IsChecked = false;
+            BlankButton.IsChecked = false;
+           
+            
+        }
+
+        private void checkButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(twoPage));
+            chooseRadioButton.IsChecked = false;
+            BlankButton.IsChecked = false;
+        }
+
+        private void BlankButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(threePage));
+            chooseRadioButton.IsChecked = false;
+            checkButton.IsChecked = false;
+        }
+    }
+}
